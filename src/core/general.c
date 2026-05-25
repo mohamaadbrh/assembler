1ec14be5b82da30d90501c3c4fbe621dc5a6f89b
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2026-05-25 22:21:28 +0330

    Fixed a bug in line parsing

--- src/core/general.c
+++ src/core/general.c
@@ -4,11 +4,6 @@
 InstructionInfo parseLine(const char *line)
 {
     InstructionInfo result;
-    result.label[0] = '\0';
-    result.arguments[0] = 0;
-    result.arguments[1] = 0;
-    result.arguments[2] = 0;
-    result.function[0] = '\0';
     int tokens = sscanf(line, "%s %s %d,%d,%d",
            result.label,
            result.function,
