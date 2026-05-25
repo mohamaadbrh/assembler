4ec3e123878cce91ad02930e30d1466068dfab58
amirmohsen1384 <amirmohsen84ghasemi@gmail.com>
2026-05-25 20:22:21 +0330

    Implemented the hash table

--- src/core/table.c
+++ src/core/table.c
@@ -1,1 +1,41 @@
 #include "table.h"
+
+int getLabelHash(const char *label, size_t size)
+{
+    int result = 0;
+    for (int counter = 0; counter < strlen(label); ++counter)
+    {
+        result = result * 7 + label[counter];
+    }
+    return result % size;
+}
+
+int insertLabel(Symbol *table, size_t size, Symbol value)
+{
+    int position = getHash(value.label, size);
+    while(table[position].used == SYMBOL_USED)
+    {
+        if(!strncmp(table[position].label, value.label, LABEL_LENGTH))
+        {
+            return INSERT_FAILED;
+        }
+        position = (position + 1) % size;
+    }
+    value.used = SYMBOL_USED;
+    table[position] = value;
+    return position;
+}
+
+int findLabel(Symbol *table, size_t size, const char *label)
+{
+    int position = getHash(label, size);
+    while(table[position].used == SYMBOL_USED)
+    {
+        if(!strncmp(table[position].label, label, LABEL_LENGTH))
+        {
+            return position;
+        }
+        position = (position + 1) % size;
+    }
+    return FIND_FAILED;
+}
--- src/core/table.h
+++ src/core/table.h
@@ -1,4 +1,27 @@
 #ifndef TABLE_H
 #define TABLE_H
 
+#include <string.h>
+#include <stdint.h>
+
+#define SYMBOL_USED 1
+#define LABEL_LENGTH 8
+#define SYMBOL_UNUSED 0
+
+typedef struct Symbol
+{
+    char label[LABEL_LENGTH];
+    int32_t used;
+    int32_t pc;
+}
+Symbol;
+
+int getLabelHash(const char *label, size_t size);
+
+#define INSERT_FAILED -1
+int insertLabel(Symbol *table, size_t size, Symbol value);
+
+#define FIND_FAILED -1
+int findByLabel(Symbol *table, size_t size, const char *label);
+
 #endif
